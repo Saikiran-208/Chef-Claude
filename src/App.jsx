20c29dd403917ae780e790ad26e8b5fde72ed9a1
saikiran <saikirankiran555.com>
2025-09-17 17:40:10 +0530

    Fix import path for Main component removed unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import Main from './components/main'
-import Test from './components/practice'
+import Main from './components/Main'
+
 
 function App() {
   
@@ -9,7 +9,7 @@ function App() {
     <>
     <Navbar />
     <Main  />
-    {/* <Test /> */}
+   
    
     </>
   )
