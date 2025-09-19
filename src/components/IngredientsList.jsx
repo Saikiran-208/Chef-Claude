855f286918e2f53c7b99c6c5b89ef0f97f3964cb
Saikiran Danamaina <145547192+Saikiran-208@users.noreply.github.com>
2025-09-19 17:34:58 +0530

    Update IngredientsList.jsx

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const IngredientsList = ( {ingredients,getRecipe} ) => {
+const IngredientsList = ( {ingredients,getRecipe,ref} ) => {
     return (
         <>
             <div className=' px-2 py-4 mt-[-10px]  max-sm:mx-10 '>
@@ -19,7 +19,7 @@ const IngredientsList = ( {ingredients,getRecipe} ) => {
 
 
             {
-                ingredients.length > 3 && <div className="flex gap-10 bg-yellow-50 px-2 py-5 w-150 max-sm:max-w-3xs max-sm:min-w-sm rounded-md shadow justify-center items-center">
+                ingredients.length > 3 && <div ref={ref} className="flex gap-10 bg-yellow-50 px-2 py-5 w-150 max-sm:max-w-3xs max-sm:min-w-sm rounded-md shadow justify-center items-center">
                     <div className="flex flex-col gap-3 max-sm:gap-1">
                         <p className="font-semibold text-lg max-sm:text-sm">Ready for a recipe?</p>
                         <p className="text-gray-500 max-sm:text-xs">Generate a recipe from your list of ingredients.</p>
